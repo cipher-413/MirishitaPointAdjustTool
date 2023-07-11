2bbfb338e804334bb760cc12247875cd2b16ef80
ちぺら <cipher.413.on.net@gmail.com>
2023-07-11 19:38:14 +0900

    Update MirishitaPointAdjustTool.cpp

--- MirishitaPointAdjustTool.cpp
+++ MirishitaPointAdjustTool.cpp
@@ -1,8 +1,3 @@
-﻿
-#define PT_A 515        //ポイントA
-#define PT_B 441        //ポイントB
-
-#define REMAIN 6766    //調整目標までの残ポイント
 
 #include <iostream>
 
@@ -10,18 +5,51 @@ int main(void)
 {
     int a = 1;          //aのプレイ回数
     int b = 1;          //bのプレイ回数
+
+    int ptA = 0;        //ライブAのポイント
+    int ptB = 0;        //ライブBのポイント
+
+    int target = 0;     //目標ポイント
+    int current = 0;    //現在のポイント
+    int remain = 0;     //目標への残
+
+    char f;             //終了判定
+
     int surplus = 1;    //剰余
 
-    while (surplus != 0) {
-        surplus = (REMAIN - PT_A * a) % PT_B;
-        a++;
+    std::cout << " input target point> ";
+    std::cin >> target;
 
+    std::cout << " input current point> ";
+    std::cin >> current;
+
+    remain = target - current;
+
+    while (1) {
+        std::cout << " input pt of live A> ";
+        std::cin >> ptA;
+
+        std::cout << " input pt of live B> ";
+        std::cin >> ptB;
+
+        while (surplus != 0) {
+            surplus = (remain - ptA * a) % ptB;
+            a++;
+
+        }
+
+        b = (remain - ptA * (a - 1)) / ptB;
+
+        std::cout << " a: " << a - 1 << " b: " << b << std::endl;
+
+        std::cout << " continue? (y/n) > ";
+        std::cin >> f;
+        if (f == 'n')break;
+
+        surplus = 1;
+        a = 1;
+        b = 1;
     }
-
-    b = (REMAIN - PT_A * (a - 1)) / PT_B;
-
-    std::cout << a - 1 << " " << b << std::endl;
-
 
     return 0;
 }
